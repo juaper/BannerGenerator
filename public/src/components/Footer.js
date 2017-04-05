856837e3e765784e7cd6badc6e1e986319b55391
Nir Benya <nirb@gefen.online>
2017-04-05 19:47:30 +0300

    commit

--- public/src/components/Footer.js
+++ public/src/components/Footer.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default ()=>{
 
     const setCookie = (()=>{
-        document.cookie+= 'name=nir; ';
+        document.cookie = 'name=nir; ';
         console.log(document.cookie);
     })();
 
